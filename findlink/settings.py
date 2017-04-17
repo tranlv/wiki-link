9b1d936fb4cf6be3d99e07d186f773f27d346431
Tran Ly Vu <vutransingapore@gmail.com>
2017-04-17 16:37:03 +0800

    .

--- findlink/settings.py
+++ findlink/settings.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine  = create_engine('mysql+pymysql://root:13061990@localhost:32782', pool_recycle=3600)
+
+engine  = create_engine('mysql+pymysql://username:password@ip:port', pool_recycle=3600)
 Session = sessionmaker(bind=engine)
 session = Session()
