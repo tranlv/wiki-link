6fd82b22ad788556b62fac522161aa532c3692f8
Tran Ly Vu <vutransingapore@gmail.com>
2017-04-18 17:39:55 +0800

    fix database

--- findlink/settings.py
+++ findlink/settings.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-username = "abc"
-password = "abc"
-ip = "abc"
-port = "abc"
-engine = create_engine("mysql+pymysql://'%s':'%s'@'%s':'%s'", pool_recycle=3600) % (
-                     username, password, ip, port)
+username = "sample"
+password = "sample"
+ip = "localhost"
+port = 3310
+engine = create_engine("mysql+pymysql://'%s':'%s'@'%s':'%s'" % (
+                     username, password, ip, port))
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
