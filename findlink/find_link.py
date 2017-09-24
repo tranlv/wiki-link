9e785a06010f48ffba40855fa1734808df1dcc78
Vu TranLy <vutransingapore@gmail.com>
2017-09-24 23:53:36 +0800

    fix find_link
    Signed-off-by: Vu TranLy <vutransingapore@gmail.com>

--- findlink/find_link.py
+++ findlink/find_link.py
@@ -128,7 +128,7 @@ class FindLink:
         self.data_handle = DataHandle()
         self.data_handle.update_page_if_not_exists(starting_url)
         self.data_handle.update_page_if_not_exists(ending_url)
-        
+
         # update link for starting_page
         starting_id = session.query(Page.id).filter(Page.url == starting_url).all()
         DataHandle.update_link(starting_id[0], starting_id[0], 0)
