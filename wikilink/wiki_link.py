d593baaa7abd8d0ac70718f8babf096807be96be
Tran Ly Vu <vutransingapore@gmail.com>
2017-10-20 23:22:01 +0800

    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>
    	modified:   wikilink/wiki_link.py

--- wikilink/wiki_link.py
+++ wikilink/wiki_link.py
@@ -3,12 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 import re
 from requests import get, HTTPError
 from bs4 import BeautifulSoup
-from sqlalchemy.orm import sessionmaker,Session
+from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base() #metadata
-existed_url = set()
-
-
 class Link(Base):
     __tablename__ = 'links'
 
@@ -33,23 +31,30 @@ class Page(Base):
         return "<Page(url ='%s', created='%s')>" %(self.url, self.created)
 
 engine = create_engine('mysql://root:12345@192.168.99.100:32771', echo=True)
-# create tables
-Base.metadata.create_all(bind=engine)
-session = Session()
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
-
+existed_url = set()
+session = Session() # having conversation with database
 
 class DataHandle:
     def __init__(self):
         return
 
     def retrieve_data(self, ending_url, number_of_separation):
+        '''
+
+        :param ending_url:
+        :param number_of_separation:
+        :return: boolean
+        '''
+
         global existed_url
 
-        # query all the page where ending page is the parameter
+        # query all the page id where to_page id is the parameter
+        # when separation is 0, the ending page retrieve itself
         from_page_id_list = session.query(Link.from_page_id).filter(
-                                Link.number_of_separation == number_of_separation - 1,
-                                Link.to_page_id == ending_url).all()
+                                          Link.number_of_separation == number_of_separation,
+                                          Link.to_page_id == ending_url).all()
 
         for url_id in from_page_id_list:
 
@@ -70,23 +75,25 @@ class DataHandle:
             # update all wiki links with tag 'a' and attribute 'href' start with '/wiki/'
             for link in soup.findAll("a", href=re.compile("^(/wiki/)[^:#]")):
 
-                # only insert link starting with /wiki/
+                # only insert link starting with /wiki/ and update Page if not exist
                 inserted_url = link.attrs['href']
-
-                # update page table
-                if inserted_url not in existed_url:
-                    self.update_page_if_not_exists(inserted_url)
+                self.update_page_if_not_exists(inserted_url)
 
                 # update links table
                 inserted_id = session.query(Page.id).filter(Page.url == inserted_url).first()
-                self.update_link(int(url_id[0]), int(inserted_id[0]), number_of_separation)
+                self.update_link(int(url_id[0]), int(inserted_id[0]), 1)
 
                 if inserted_url is ending_url:
                     return True
         return False
 
     def update_page_if_not_exists(self, url):
-        # global existed_url
+        ''' insert into table Page if not exist
+
+        :param url:
+        :return: null
+        '''
+
         page_list = session.query(Page).filter(Page.url == url).all()
         if page_list.len() == 0:
             existed_url.add(url)
@@ -95,9 +102,16 @@ class DataHandle:
             session.commit()
 
     def update_link(self, from_page_id, to_page_id, no_of_separation):
-        link_between_2_pages = session.query(Link).\
-                                filter(Link.from_page_id == from_page_id,
-                                       Link.to_page_id == to_page_id).all()
+        ''' insert into table Link if link has not existed
+
+        :param from_page_id:
+        :param to_page_id:
+        :param no_of_separation:
+        :return: null
+        '''
+
+        link_between_2_pages = session.query(Link).filter(Link.from_page_id == from_page_id,
+                                                          Link.to_page_id == to_page_id).all()
         if link_between_2_pages.len() == 0:
             link = Link(from_page_id=Link.from_page_id,
                         to_page_id=to_page_id,
@@ -113,14 +127,23 @@ class Searcher:
         self.my_list = []
 
     def link_search(self, current_page, starting_page):
+        '''
+        :param current_page:
+        :param starting_page:
+        :return:
+        '''
         while starting_page not in self.my_list:
+            # retrieve entry in Page with current url
             current_url_id = session.query(Page.id).filter(Page.url == current_page).first()
 
+            # retrieve the the shortest path to the current url using id
             min_separation = session.query(func.min(Link.number_of_separation)).filter(Link.to_page_id == current_url_id[0])
 
+            # retrieve all the id of pages which has min no of separation to current url
             from_page_id = session.query(Link.from_page_id).filter(Link.to_page_id == current_url_id[0], Link.
                                                                    number_of_separation == min_separation)
 
+            #
             url = session.query(Page.url).filter(Page.id == from_page_id[0]).first()
             if url[0] not in self.my_list:
                 self.my_list.append(url[0])
@@ -138,7 +161,7 @@ class WikiLink:
         self.starting_url = starting_url
         self.ending_url = ending_url
         self.found = False
-        self.number_of_separation = 1
+        self.number_of_separation = 0
 
         self.data_handle = DataHandle()
         self.search = Searcher(self.starting_url, self.ending_url)
@@ -147,23 +170,23 @@ class WikiLink:
         self.data_handle.update_page_if_not_exists(starting_url)
         self.data_handle.update_page_if_not_exists(ending_url)
 
-        # update link for starting_page
+        # update link for starting_page, no of separation between 1 url to itself is zero of course
         starting_id = session.query(Page.id).filter(Page.url == starting_url).all()
         self.data_handle.update_link(starting_id[0], starting_id[0], 0)
 
     def search(self):
-        """
-        print out number of separation
-        """
+        '''
 
-        self.found = self.data_handle.retrieve_data(self.ending_url, self.number_of_separation)
+        :return:
+        '''
 
         while self.found is False:
-            self.number_of_separation += 1
-            if self.number_of_separation > self.limit:
-                print ("Number of separation is exceeded number of limit. Stop searching!")
-                return
             self.found = self.data_handle.retrieve_data(self.ending_url, self.number_of_separation)
+            if self.number_of_separation > self.limit:
+                print ("Number of separation is exceeded number of limit...Stop searching!")
+                return
+            elif self.found is False:
+                self.number_of_separation += 1
 
         print ("Smallest number of separation is " + str(self.number_of_separation))
 
@@ -173,7 +196,7 @@ class WikiLink:
         """
 
         if self.number_of_separation > self.limit or self.found is False:
-            print ("No solution within limit!")
+            print ("No solution within limit! Consider to raise the limit.")
             return
 
         my_list = [self.ending_url] + self.search.list_of_links()
