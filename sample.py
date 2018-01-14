dc344ae85dfeaa8abda82a1bb2188bd7e3703e2b
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-14 20:48:22 +0800

    Update sample file
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- sample.py
+++ sample.py
@@ -5,6 +5,5 @@ def main():
 	ending_url = '/wiki/Bill_Clinton'
 	model = WikiLink(starting_url, ending_url)
 	print("Smallest number of separation is " + str(model.search()))
-	model.print_links()
 
 if __name__ == "__main__": main()
