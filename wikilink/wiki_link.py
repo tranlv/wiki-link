4bebc772c6890625f2b3d5a614b91aa11e6f8331
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-14 16:39:04 +0800

    Rename configuration file to include udnerscore
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- wikilink/wiki_link.py
+++ wikilink/wiki_link.py
@@ -175,7 +175,7 @@ class WikiLink:
 def main():
 	starting_url = '/wiki/Barack_Obama'
 	ending_url = '/wiki/Bill_Clinton'
-	model = WikiLink(starting_url,ending_url)
+	model = WikiLink(starting_url, ending_url)
 	#model.search()
 	#model.print_links()
 
