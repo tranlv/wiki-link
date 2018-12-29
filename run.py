d53fd928aa8160d7a1ed1ccb3005527828c3d18e
Tran Ly Vu <tranly.vu@accellion.com>
2018-12-29 15:04:17 +0800

    Add user input

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ from wikilink.wiki_link import WikiLink
 def main():
 	starting_url = input("Starting url: ")
 	ending_url = input("Ending url: ")
+	limit = int(input("limit (default=6): ") or "42")
 	model = WikiLink(starting_url, ending_url)
-	print("Smallest number of separation is " + str(model.search()))
+	if model.search() is not None: 
+		answer = "Smallest number of separation is " + str(model.search())		
+		print(answer)
 
 if __name__ == "__main__": 
 	main()
