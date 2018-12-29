7406d6d9761e7968f902156da6ac2d95da801c92
Tran Ly Vu <tranly.vu@accellion.com>
2018-12-29 15:57:35 +0800

    Add post method to index
    Signed-off-by: Tran Ly Vu <tranly.vu@accellion.com>

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from wikilink.wiki_link import WikiLink
 def main():
 	starting_url = input("Starting url: ")
 	ending_url = input("Ending url: ")
-	limit = int(input("limit (default=6): ") or "42")
+	limit = int(input("limit (default=6): ") or "6")
 	model = WikiLink(starting_url, ending_url)
 	if model.search() is not None: 
 		answer = "Smallest number of separation is " + str(model.search())		
--- web/index.py
+++ web/index.py
@@ -1,16 +1,30 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request,render_template
+from wikilink.wiki_link import WikiLink
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def main():
-	return hello('vu')
+	return render_template('hello.html')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route("/", methods=['POST'])
+def main_post():
+	try :
+		starting_link = request.form['Starting wiki link']
+		ending_link = request.form['Ending wiki link']
+	except:
+		#to do
+
+	if request.form('Limit') == ''
+		limit ==  6
+	else: 
+		limit = request.form('Limit')
+
+	model = WikiLink(starting_url, ending_url)
+	if model.search() is not None: 
+		answer = "Smallest number of separation is " + str(model.search())		
+		print(answer)
+	return 
 
 if __name__ == "__main__":
     app.run()
