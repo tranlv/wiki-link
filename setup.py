4d5268efa88d08be040f29186c06d4c0fb4c5108
Tran Ly Vu <vutransingapore@gmail.com>
2019-01-12 22:01:08 +0800

    Update packages
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- setup.py
+++ setup.py
@@ -1,38 +1,54 @@
 """
-WikiLink
------
-	WikiLink is web-scraping application to find minimum number of links between two given wiki pages. It's extensively documented
-    and follows best practice patterns.
-    
-	:copyright: (c) 2016 by Tran Ly VU.
-    :license: Apache License 2.0, see LICENSE for more details.
-"""
-from setuptools import setup
+	wiki-link
+	~~~~~~~~
 
+	wiki-link is a web-scraping application to find minimum number 
+	of links between two given wiki pages. 
+    
+    :copyright: (c) 2016 - 2018 by Tran Ly VU. All Rights Reserved.
+    :license: Apache License 2.0.
+"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-	name='WikiLink',
-	description='A web-scraping application to find the minimum number of links between 2 given wiki pages',
+	name='wikilink',
+	version="1.2.0",
 	author='Tran Ly Vu',
 	author_email='vutransingapore@gmail.com',
+	description='A web-scraping application to find the minimum number of links between 2 given wiki pages',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/tranlyvu/wiki-link",
+	packages=find_packages("wikilink"),
+	package_dir={'':'wikilink'},
 	license='Apache License 2.0',
 	classifiers=[
-		'Development Status :: 3 - Alpha',
+		"Programming Language :: Python",
+		"License :: OSI Approved :: Apache Software License ",
+		"Operating System :: OS Independent",
+		"Development Status :: 5 - Production/Stable",
+		"Natural Language :: English",
+		"Environment :: Console",
 		'Intended Audience :: Science/Research',
-		'Framework :: IPython'
 		'Topic :: Scientific/Engineering :: Artificial Intelligence',
-		'Programming Language :: Python :: 3.6',
-		'Operating System :: Microsoft :: Windows :: Windows 10',
-		'Natural Language :: English',
-		'License :: OSI Approved :: Apache License 2.0',
 	],
-	keywords='web-scraping',
-	packages=['wiki-link'],
+	keywords=["web-scraping", "Artificial Intelligence", "breadth first search", "Graph"],
+	project_urls={
+    'Source': 'https://github.com/tranlyvu/wiki-link',
+    'Tracker': 'https://github.com/tranlyvu/wiki-link/issues',
+	},
 	install_requires=[
-		'beautifulsoup4'
+		'beautifulsoup4',
 		'bs4',
-		'SQLAlchemy'
-		'requests'
-		]
-				
+		'SQLAlchemy',
+		"SQLAlchemy-Utils",
+		'requests',
+		'Flask',
+		"mysqlclient",
+		"Jinja2"
+		],
+	python_requires="~=3.6"			
 )
