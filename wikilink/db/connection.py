57bee62b1bd6c42469f420e036937ea3c9ca56b7
Tran Ly Vu <vutransingapore@gmail.com>
2019-01-16 00:38:29 +0800

    Change class name to Connection
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

---
+++ wikilink/db/connection.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy_utils import functions
+from sqlalchemy.orm import sessionmaker
+
+class Connection:
+	def __init__(self, db, name, password, ip, port):
+		if db == "postgresql":
+			connection = "postgresql+psycopg2://" + name + ":" + password + "@" + ip + ":" + port			
+		elif db == "mysql":
+			connection = "mysql+pymysql://" + name + ":" + password + "@" + ip + ":" + port
+		
+		db_name = 'wikilink'
+		# Turn off echo
+		engine = create_engine(connection + "/" + db_name + '?charset=utf8', echo=False, encoding='utf-8')
+		if not functions.database_exists(engine.url):
+			functions.create_database(engine.url)
+
+		self.session = sessionmaker(bind=engine)()
+		# If table don't exist, Create.
+		if (not engine.dialect.has_table(engine, 'link') and not engine.dialect.has_table(engine, 'page')):
+			Base.metadata.create_all(engine)
